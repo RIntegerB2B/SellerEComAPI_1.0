46a3d50512edf9ab6a7218e650490c2fb45db5c0
1941h3a1thcar3 <preveenchandran@gmail.com>
2018-03-23 17:18:51 +0530

    Category Changes

--- api/models/category.js
+++ api/models/category.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 
 const CategorySchema = new mongoose.Schema({
     _id: { type: Number, default: 1 },
+    categoryId:Number,
+    headerCategory: Number,
     categoryName: String,
     subCategories: [{_id:Number, subCategoryName:String}]
   });
