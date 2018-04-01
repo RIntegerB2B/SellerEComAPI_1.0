8d4a944c5c89e7e511b69f6011c71d6071697bb7
1941h3a1thcar3 <preveenchandran@gmail.com>
2018-04-01 18:25:21 +0530

    Create Admin

--- api/adminAccount/adminAccountRoute.js
+++ api/adminAccount/adminAccountRoute.js
@@ -2,7 +2,8 @@
 var adminLoginMgr = require('./adminLogin/adminLoginMgr');
 module.exports = function(app) {
     
-   
+    app.route('/admin/create')
+    .post(adminLoginMgr.create);
 
     app.route('/admin')
     .post(adminLoginMgr.signInToSite);
