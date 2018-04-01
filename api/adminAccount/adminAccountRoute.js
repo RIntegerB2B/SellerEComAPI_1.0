bb3ed59b5e5f9af8b2e218c0267fa2dc1e748a69
1941h3a1thcar3 <preveenchandran@gmail.com>
2018-04-01 18:12:38 +0530

    asdasd

--- api/adminAccount/adminAccountRoute.js
+++ api/adminAccount/adminAccountRoute.js
@@ -2,8 +2,7 @@
 var adminLoginMgr = require('./adminLogin/adminLoginMgr');
 module.exports = function(app) {
     
-    app.route('/admin/create')
-    .post(adminLoginMgr.create);
+   
 
     app.route('/admin')
     .post(adminLoginMgr.signInToSite);
